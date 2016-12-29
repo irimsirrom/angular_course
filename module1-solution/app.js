498481858d90abcfb5f1db9096c8c5833c24ecf9
irimsirrom <irimsirrom@gmail.com>
2016-12-29 14:51:07 +0200

    Add files via upload

---
+++ module1-solution/app.js
@@ -0,0 +1,46 @@
+(function () {
+'use strict';
+
+angular.module('lunchCheckerApp', [])
+.controller('LunchCheckerController', LunchCheckerController);
+
+LunchCheckerController.$inject = ['$scope'];
+function LunchCheckerController($scope){
+
+    $scope.list = "";
+    $scope.countItems = function() {
+        var listSplitting = makeArray($scope.list);
+        listSplitting = listSplitting.filter(function(array) { return array.trim() != ''; });
+        $scope.itemCount = listSplitting.length;
+        $scope.message = "";
+        if ($scope.itemCount === 0){
+            $scope.message = "Please enter data";
+        }
+
+        if($scope.itemCount < 3 && $scope.itemCount != 0){
+            $scope.message = "Enjoy";
+        }
+
+        if($scope.itemCount > 3){
+            $scope.message = "Too Much!";
+        }
+
+
+    };
+
+    function makeArray(string) {
+        return string.split(" ");
+    }
+
+
+   $scope.outputMessage = function(){
+
+       
+   } 
+
+
+}
+
+})();
+
+
